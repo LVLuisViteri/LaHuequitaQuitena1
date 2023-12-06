707d0ef5faabe085516064e513ba56a479b3c565
LVLuisViteri <luisviteri1990@gmail.com>
2023-12-06 17:20:05 -0300

    Alt

--- main.js
+++ main.js
@@ -1477,7 +1477,7 @@ function cargarProductos(productosElegidos){
         const div = document.createElement("div");
         div.classList.add("producto")
         div.innerHTML = `
-            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.alt}">
             <div class="producto-detalle">
                 <h3 class="producto-titulo">${producto.titulo}</h3>
                 <p class="producto-precio">$${producto.precio}</p>
